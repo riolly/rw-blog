171b7d7415fcdc555108c0375570ac577e2b3922
Riolly <alberto.r@achiko.co>
2022-02-01 19:17:20 +0700

    add layout

--- web/src/pages/AboutPage/AboutPage.js
+++ web/src/pages/AboutPage/AboutPage.js
@@ -4,24 +4,10 @@ import { MetaTags } from '@redwoodjs/web'
 const AboutPage = () => {
   return (
     <>
-       <header>
-        <nav>
-          <ul>
-            <li>
-              <Link to={routes.about()}>About</Link>
-            </li>
-            <li>
-              <Link to={routes.home()}>Home</Link>
-            </li>
-          </ul>
-        </nav>
-      </header>
-      <main>
-        <p>
-          This site was created to demonstrate my mastery of Redwood: Look on my
-          works, ye mighty, and despair!
-        </p>
-      </main>
+      <p>
+        This site was created to demonstrate my mastery of Redwood: Look on my
+        works, ye mighty, and despair!
+      </p>
     </>
   )
 }
--- web/src/pages/HomePage/HomePage.js
+++ web/src/pages/HomePage/HomePage.js
@@ -3,21 +3,7 @@ import { Link, routes } from '@redwoodjs/router'
 const HomePage = () => {
   return (
     <>
-      <header>
-        <nav>
-          <ul>
-            <li>
-              <Link to={routes.about()}>About</Link>
-            </li>
-            <li>
-              <Link to={routes.home()}>Home</Link>
-            </li>
-          </ul>
-        </nav>
-      </header>
-      <main>
-        <p>This is home page. Welcome Home!</p>
-      </main>
+      <p>This is home page. Welcome Home!</p>
     </>
   )
 }
